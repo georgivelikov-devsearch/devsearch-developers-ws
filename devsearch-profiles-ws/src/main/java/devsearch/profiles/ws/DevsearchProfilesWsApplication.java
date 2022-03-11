447452059b3c99bfe71f7ce335e0532d273ae9d0
georgivelikov-devsearch <gmvelikov1@gmail.com>
2022-03-11 16:41:48 +0200

    Initial commit

---
+++ devsearch-profiles-ws/src/main/java/devsearch/profiles/ws/DevsearchProfilesWsApplication.java
@@ -0,0 +1,13 @@
+package devsearch.profiles.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsearchProfilesWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsearchProfilesWsApplication.class, args);
+	}
+
+}
---
+++ devsearch-profiles-ws/src/test/java/devsearch/profiles/ws/DevsearchProfilesWsApplicationTests.java
@@ -0,0 +1,13 @@
+package devsearch.profiles.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsearchProfilesWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
