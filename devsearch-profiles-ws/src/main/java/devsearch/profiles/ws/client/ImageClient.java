d5eba6c5deb51860a76fa6d8184850ccd0e0bb2f
georgivelikov-devsearch <gmvelikov1@gmail.com>
2022-03-12 13:30:32 +0200

    Adding more things to images func

--- devsearch-profiles-ws/src/main/java/devsearch/profiles/ws/client/ImageClient.java
+++ devsearch-profiles-ws/src/main/java/devsearch/profiles/ws/client/ImageClient.java
@@ -1,5 +1,16 @@
 package devsearch.profiles.ws.client;
 
-public class ImageClient {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import devsearch.profiles.ws.ui.model.request.ProfileImageRequest;
+import devsearch.profiles.ws.ui.model.response.ImageResponse;
+
+@FeignClient("clients-ws")
+public interface ImageClient {
+
+    @PostMapping("/profile")
+    public ResponseEntity<ImageResponse> addProfileImage(@RequestBody ProfileImageRequest imageRequest);
 }
