bdd9477311c49cfc0de6c433b76a70f2d9d72f6b
georgivelikov-devsearch <gmvelikov1@gmail.com>
2022-03-13 11:47:28 +0200

    Fixing github token

--- devsearch-profiles-ws/src/main/java/devsearch/profiles/ws/client/ImageClient.java
+++ devsearch-profiles-ws/src/main/java/devsearch/profiles/ws/client/ImageClient.java
@@ -11,8 +11,6 @@ import devsearch.profiles.ws.ui.model.response.ImageResponse;
 @FeignClient("images-ws")
 public interface ImageClient {
 
-    String x;
-
     @PostMapping("/images/profile")
     public ResponseEntity<ImageResponse> addProfileImage(@RequestBody ProfileImageRequest imageRequest);
 }
