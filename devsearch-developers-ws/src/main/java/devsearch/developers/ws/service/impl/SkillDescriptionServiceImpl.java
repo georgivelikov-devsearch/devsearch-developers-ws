33dd41320f55c1d9ba82781090f80fc8488f0c85
georgivelikov-devsearch <gmvelikov1@gmail.com>
2022-03-26 21:56:03 +0200

    Adding SkillDescriptionServiceImpl

--- devsearch-developers-ws/src/main/java/devsearch/developers/ws/service/impl/SkillDescriptionServiceImpl.java
+++ devsearch-developers-ws/src/main/java/devsearch/developers/ws/service/impl/SkillDescriptionServiceImpl.java
@@ -1,25 +1,47 @@
 package devsearch.developers.ws.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import devsearch.developers.ws.exception.RestApiDevelopersException;
+import devsearch.developers.ws.io.entity.SkillDescriptionEntity;
+import devsearch.developers.ws.io.repository.SkillDescriptionRepository;
 import devsearch.developers.ws.service.SkillDescriptionService;
 import devsearch.developers.ws.shared.dto.SkillDescriptionDto;
+import devsearch.developers.ws.shared.utils.AppConstants;
+import devsearch.developers.ws.shared.utils.Mapper;
+import devsearch.developers.ws.shared.utils.Utils;
 
 @Service
 public class SkillDescriptionServiceImpl implements SkillDescriptionService {
 
+    @Autowired
+    private SkillDescriptionRepository skillDescriptionRepository;
+
+    @Autowired
+    private Mapper modelMapper;
+
+    @Autowired
+    private Utils utils;
+
     @Override
     public SkillDescriptionDto getSkillDescriptionBySkillDescriptionId(String skillDescriptionId) {
-	// TODO Auto-generated method stub
-	return null;
+	SkillDescriptionEntity skillDescriptionEntity = skillDescriptionRepository
+		.findBySkillDescriptionId(skillDescriptionId);
+
+	return modelMapper.map(skillDescriptionEntity, SkillDescriptionDto.class);
     }
 
     @Override
-    public SkillDescriptionDto createSkillDescriptionDto(SkillDescriptionDto skillDescription)
+    public SkillDescriptionDto createSkillDescriptionDto(SkillDescriptionDto skillDescriptionDto)
 	    throws RestApiDevelopersException {
-	// TODO Auto-generated method stub
-	return null;
+	SkillDescriptionEntity skillDescriptionEntity = modelMapper.map(skillDescriptionDto,
+		SkillDescriptionEntity.class);
+	skillDescriptionEntity.setSkillDescriptionId(utils.generatePublicId(AppConstants.PRIVATE_ID_LENGTH));
+
+	SkillDescriptionEntity storedEntity = skillDescriptionRepository.save(skillDescriptionEntity);
+
+	return modelMapper.map(storedEntity, SkillDescriptionDto.class);
     }
 
 }
